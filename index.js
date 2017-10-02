4cca1648585411733a751a063096be5a1672a4c5
DileepChittamuru <chdileep8500@gmail.com>
2017-10-02 14:06:56 +0530

    TNFE-02 updated port number

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var express = require('express');
 var app = express();
 
 app.use('/',express.static(__dirname + '/'));
-
-app.listen(3000, function() {
-	console.log("running on 3000")
+app.set('port', (process.env.PORT || 5000));
+app.listen(app.get('port'), function() {
+	console.log("running on", app.get('port'));
 });
